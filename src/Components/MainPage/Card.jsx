56d4428605bb1ebc6cf897348ae1e0855bef59c7
AhmedK2151 <113545516+AhmedK2151@users.noreply.github.com>
2022-11-25 14:51:14 +0000

    Synced

--- src/Components/MainPage/Card.jsx
+++ src/Components/MainPage/Card.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 const Card=(movie)=>{
    
+    const [movieImg, setMovieImg] = useState()
+
+    useEffect(() => {
+        if (movie.info.poster_path) {
+            const moviePoster = movie.info.poster_path
+            setMovieImg("https://image.tmdb.org/t/p/w500" + moviePoster)
+        } else if(!movie.info.poster_path){
+            const moviePoster = "https://img.icons8.com/carbon-copy/100/null/no-image.png" 
+            setMovieImg(moviePoster)
+        }
+    })
+
     let img_path="https://image.tmdb.org/t/p/w500";
     return(
         <>
             <div className="movie">
-                <img src={img_path+movie.info.poster_path} className="poster" alt="poster-img"></img>
+                <img src={movieImg} className="poster" alt="poster-img"></img>
                 <div className="movie-details">
                     <div className="box">
                         <h4 className="title">{movie.info.title}</h4>
